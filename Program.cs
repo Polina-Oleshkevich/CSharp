187b56abf87c6bfb426ee0bdd50aa7c7c41af0bb
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2021-11-24 23:00:14 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,70 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
 
-namespace Passwordgenerator
+namespace PasswordGenerator2
 {
-    class Program
+    class PasswordGenerator
     {
-        static void Main()
+        private static char GetRandomUCase(Random r)
         {
-            Console.WriteLine("Какой длины пароль?");
-            GenertorPw.Length = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Пароль: {0}", GenertorPw.PasswdGetSet());
-            Console.ReadLine();
+            return (char)(r.Next(0, 26) + 'A');
+        }
+
+        private static char GetRandomLCase(Random r)
+        {
+            return (char)(r.Next(0, 26) + 'a');
+        }
+
+        private static char GetRandomDigit(Random r)
+        {
+            return (char)(r.Next(0, 9) + '0');
+        }
+        public static string GetPassword()
+        {
+            char[] passwordChars = new char[8];
+            Random r = new Random();
+            for (int i = 0; i < passwordChars.Length; i++)
+                switch (r.Next(0, 3))
+                {
+                    case 0:
+                        passwordChars[i] = GetRandomDigit(r);
+                        break;
+                    case 1:
+                        passwordChars[i] = GetRandomLCase(r);
+                        break;
+                    case 2:
+                        passwordChars[i] = GetRandomUCase(r);
+                        break;
+                    default:
+                        break;
+                }
+
+            int digitPos = r.Next(0, passwordChars.Length);
+            passwordChars[digitPos] = GetRandomDigit(r);
+
+            int lCasePos = new int();
+            do
+                lCasePos = r.Next(0, passwordChars.Length);
+            while (lCasePos == digitPos);
+            passwordChars[lCasePos] = GetRandomLCase(r);
+
+            int uCasePos = new int();
+            do
+                uCasePos = r.Next(0, passwordChars.Length);
+            while (uCasePos == digitPos || uCasePos == lCasePos);
+            passwordChars[uCasePos] = GetRandomUCase(r);
+
+            string password = new string(passwordChars);
+            return password;
         }
     }
-    class GenertorPw
+    class Program
     {
-        static public int Length { get; set; }
-        static public string PasswdGetSet()
+        static void Main(string[] args)
         {
-            Random random = new Random();
-            StringBuilder Passwd = new StringBuilder();
-            for (int i = 0; i < Length; i++)
-            {
-                Passwd.Append((char)random.Next(50, 100));
-            }
-            return Passwd.ToString();
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 50; ++i)
+                Console.WriteLine(PasswordGenerator.GetPassword());
         }
     }
 }
